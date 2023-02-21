0015e2c717f337d2e9da71bbcc50599f33c0e6a2
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-02-21 16:46:35 +0100

    FEAT input for adding task and tasks visible added

--- js/app.js
+++ js/app.js
@@ -1,10 +1,43 @@
+
+const toDoList = [
+
+]
+
 // Vue
 const { createApp } = Vue
 
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        toDoList,
+        inputTask: '', 
       }
+    },
+    methods: {
+        addTask () {
+
+            // creo la task come oggetto
+            let task = {
+                text: this.inputTask,
+                done: false,
+            }
+            // pusho l'oggetto dentro l'array
+            toDoList.push(task);
+            // resetto il valore input della task
+            this.inputTask = '';
+
+        },
+        taskEnd (oggetto) {
+            oggetto.done = true;
+            // console.log(oggetto.done);
+        }
     }
   }).mount('#app')
+
+
+
+
+
+
+
+  
